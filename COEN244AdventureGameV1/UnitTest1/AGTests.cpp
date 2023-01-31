3b3698c2280fbba9bbf68757e49b208ed3e1d0a1
Stuart Thiel <stuart.thiel@gmail.com>
2023-01-31 15:10:40 -0500

    Added code to set the relative execution of tests

--- COEN244AdventureGameV1/UnitTest1/AGTests.cpp
+++ COEN244AdventureGameV1/UnitTest1/AGTests.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../Item.h"
+#include <filesystem>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,10 +10,15 @@ namespace AdventureGameTests
 	TEST_CLASS(ItemTests)
 	{
 	public:
-		
+
+		TEST_CLASS_INITIALIZE(SetUp) {
+			auto path = std::filesystem::current_path(); //getting path
+			std::filesystem::current_path(path.append("../../..")); //setting path
+		}
+
 		TEST_METHOD(SwordTest)
 		{
-			Item item("../../../item/sword.txt");
+			Item item("item/sword.txt");
 
 			Assert::AreEqual(std::string("Sword"), item.getName());
 		}
